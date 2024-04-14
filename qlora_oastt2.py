67acbf729264d9655eaae9c8686be59071f4e941
Maxim Saplin <smaxmail@gmail.com>
2024-04-14 13:20:17 +0300

    lmsys dataset + neftune

--- qlora_oastt2.py
+++ qlora_oastt2.py
@@ -7,21 +7,27 @@ from transformers import (
 )
 from peft import LoraConfig
 from trl import SFTTrainer
-from datasets import load_dataset, concatenate_datasets, Dataset
-import torch, wandb
+from datasets import load_dataset, concatenate_datasets, Dataset, DatasetDict
+import torch
+import wandb
 from datetime import datetime
 import time
 import numpy as np
+from enum import IntFlag
 
 
 def train():
     run_id = f"qlora-{datetime.now().strftime('%Y%m%d%H%M%S')}"
     resume = True
-    model_path = "qlora_oastt2\out_qlora-20240411163040\checkpoint-387"  # "stabilityai/stablelm-2-1_6b"
-    max_tokens = 1024 # determines the cap on max tokens in training, used in filtering of dataset
+    model_path = (
+        "out_qlora-20240411181925/checkpoint-460"  # "stabilityai/stablelm-2-1_6b"
+    )
+    max_tokens = 1024  # determines the cap on max tokens in training, used in filtering of dataset
 
     set_seed(42)
-    dataset = get_dataset(use_both_datasets=True)
+    dataset = get_dataset(
+        DatasetOptions.OASST2 | DatasetOptions.ULTRACHAT | DatasetOptions.CHATBOT_ARENA
+    )
 
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     tokenizer.chat_template = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}"
@@ -66,7 +72,7 @@ def train():
         use_cache=False,
     )
 
-    if not ("resume" in locals() and resume == True):
+    if not ("resume" in locals() and resume is True):
         lora_config = LoraConfig(
             lora_alpha=128,
             lora_dropout=0.05,
@@ -98,7 +104,9 @@ def train():
         # torch_compile=True # supposedly can make training faster, doesn't work with Linux/flash_attention
     )
 
-    print(f"Training is starting... Train records: {len(dataset['train'])}, Test records: {len(dataset['test'])}")
+    print(
+        f"Training is starting... Train records: {len(dataset['train'])}, Test records: {len(dataset['test'])}"
+    )
 
     trainer = SFTTrainer(
         model=model,
@@ -114,6 +122,7 @@ def train():
         # ),
         max_seq_length=max_tokens,
         packing=True,
+        neftune_noise_alpha=5,  # https://huggingface.co/docs/trl/en/sft_trainer#enhance-models-performances-using-neftune
         # dataset_kwargs={
         #     "add_special_tokens": False,  # We template with special tokens
         #     "append_concat_token": False,  # No need to add additional separator token
@@ -129,53 +138,99 @@ def train():
     # trainer.save_model()
 
 
-def get_dataset(use_both_datasets=False):
-    start_time = time.time()
-    print(f"Preparing dataset.... ", end="")
-    if use_both_datasets:
-        # dataset1 = load_dataset("g-ronimo/oasst2_top4k_en", split="train+test")
-        dataset1 = load_dataset("g-ronimo/oasst2_top4k_en")
-        dataset2 = load_dataset(
-            "HuggingFaceH4/ultrachat_200k", split="train_sft[:10%]+test_sft[:40%]"
-        )
+class DatasetOptions(IntFlag):
+    OASST2 = 1
+    ULTRACHAT = 2
+    CHATBOT_ARENA = 4
 
-        dataset2 = dataset2.map(
+
+def get_dataset(datasets_to_use: DatasetOptions):
+    start_time = time.time()
+    print("Preparing dataset.... ", end="")
+
+    final_dataset = DatasetDict(
+        {"train": Dataset.from_dict({}), "test": Dataset.from_dict({})}
+    )
+
+    if datasets_to_use & DatasetOptions.OASST2:
+        print("Loading oasst2...")
+        dataset = load_dataset("g-ronimo/oasst2_top4k_en")
+        concat(final_dataset, dataset)
+
+    if datasets_to_use & DatasetOptions.ULTRACHAT:
+        print("Loading ultrachat...")
+        dataset = load_dataset(
+            "HuggingFaceH4/ultrachat_200k", split="train_sft[:10%]+test_sft[:50%]"
+        )
+        # Special processing for ultrachat dataset
+        dataset = dataset.map(
             lambda example: {"messages": example["messages"]},
             batched=True,
-            remove_columns=dataset2.column_names,
+            remove_columns=dataset.column_names,
         )
-        dataset = dataset2.train_test_split(test_size=0.1)
+        # Splitting the dataset into train and test
+        dataset = dataset.train_test_split(test_size=0.1)
+        concat(final_dataset, dataset)
 
-        dataset["train"] = concatenate_datasets([dataset["train"], dataset1["train"]])
-        dataset["test"] = concatenate_datasets([dataset["test"], dataset1["test"]])
+    if datasets_to_use & DatasetOptions.CHATBOT_ARENA:
+        print("Loading chatbot_arena...")
+        dataset = load_dataset(
+            "./chatbot_arena", split="train[:100%]"
+        )  # lmsys/chatbot_arena_conversations
 
-        haddaway = Dataset.from_dict(
-            {
-                "messages": [
-                    [
-                        {
-                            "content": "What is love? Oh baby, don't hurt me...",
-                            "role": "user",
-                        },
-                        {"content": "Don't hurt me, no more.", "role": "assistant"},
-                    ]
+        # Filter for English language conversations
+        dataset = dataset.filter(lambda example: example["language"] == "English")
+
+        # Choose the winning conversation or conversation_a in case of a tie
+        def choose_winner(example):
+            if example["winner"] == "model_a" or example["winner"] == "tie":
+                return {"messages": example["conversation_a"]}
+            else:
+                return {"messages": example["conversation_b"]}
+
+        dataset = dataset.map(
+            choose_winner,
+            batched=True,
+            remove_columns=dataset.column_names,
+        )
+        dataset = dataset.train_test_split(test_size=0.1)
+        concat(final_dataset, dataset)
+
+    haddaway = Dataset.from_dict(
+        {
+            "messages": [
+                [
+                    {
+                        "content": "What is love? Oh baby, don't hurt me...",
+                        "role": "user",
+                    },
+                    {"content": "Don't hurt me, no more.", "role": "assistant"},
                 ]
-                * 10
-            }
-        )
+            ]
+            * 1
+        }
+    )
 
-        dataset["train"] = concatenate_datasets([dataset["train"], haddaway])
-        return dataset
-    else:
-        dataset = load_dataset("g-ronimo/oasst2_top4k_en")
+    final_dataset["train"] = concatenate_datasets([final_dataset["train"], haddaway])
 
     end_time = time.time()
-    print(f" Done - {end_time - start_time:.1f}s")
-    return dataset
+    print(f"Done - {end_time - start_time:.1f}s")
+    return final_dataset
+
+
+def concat(final_dataset, dataset):
+    final_dataset["train"] = concatenate_datasets(
+        [final_dataset["train"], dataset["train"]]
+    )
+    final_dataset["test"] = concatenate_datasets(
+        [final_dataset["test"], dataset["test"]]
+    )
+
 
 def filter_out_large(dataset, tokenizer, max_tokens):
     start_time = time.time()
-    print(f"Filtering out large examples.... ", end="")
+    print("Filtering out large examples.... ", end="")
+
     def filter_large_examples(example):
         tokens = tokenizer.apply_chat_template(example["messages"], tokenize=True)
         return len(tokens) <= max_tokens
@@ -184,6 +239,7 @@ def filter_out_large(dataset, tokenizer, max_tokens):
     end_time = time.time()
     print(f" Done - {end_time - start_time:.1f} seconds")
     return dataset
+
 
 def analyze_token_lengths(tokenizer, dataset, max_tokens):
     token_lengths = {"train": [], "test": []}
@@ -196,7 +252,9 @@ def analyze_token_lengths(tokenizer, dataset, max_tokens):
     for split in ["train", "test"]:
         for message in dataset[split]["messages"]:
             # tokens = tokenizer.tokenize(message) # this one only accepts text
-            tokens = tokenizer.apply_chat_template(message, tokenize=True) # turn JSON into chat markup with special tokens
+            tokens = tokenizer.apply_chat_template(
+                message, tokenize=True
+            )  # turn JSON into chat markup with special tokens
             token_lengths[split].append(len(tokens))
             if len(tokens) > max_tokens:
                 messages_over_max_tokens[split] += 1
@@ -204,7 +262,10 @@ def analyze_token_lengths(tokenizer, dataset, max_tokens):
             if count % 100 == 0:
                 print(f"Count: {count}/{total_count} \t\t", end="\r")
     end_time = time.time()
-    print(f"Count: {count}/{total_count} - {end_time - start_time:.1f} seconds", end="\t\t\r\n")
+    print(
+        f"Count: {count}/{total_count} - {end_time - start_time:.1f} seconds",
+        end="\t\t\r\n",
+    )
 
     for split in ["train", "test"]:
         lengths = token_lengths[split]
@@ -218,9 +279,12 @@ def analyze_token_lengths(tokenizer, dataset, max_tokens):
             print(f"25th percentile: {np.percentile(lengths, 25)}")
             print(f"50th percentile (median): {np.percentile(lengths, 50)}")
             print(f"75th percentile: {np.percentile(lengths, 75)}")
-            print(f"Messages over {max_tokens} tokens: {messages_over_max_tokens[split]} ({messages_over_max_tokens[split] / len(dataset[split]) * 100:.2f}%)")
+            print(
+                f"Messages over {max_tokens} tokens: {messages_over_max_tokens[split]} ({messages_over_max_tokens[split] / len(dataset[split]) * 100:.2f}%)"
+            )
         else:
             print(f"No data available for {split} split.")
+
 
 if __name__ == "__main__":
     train()
